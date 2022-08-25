64ace3dc0aa65cd5cd435a837314324e59a249e2
AyubuAyubu <ayubu6143@gmail.com>
2022-08-25 09:35:42 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let removeNonAlpha = /[\W_]/g // removes white spaces and non-alphanumeric characters
+  let newTextWord = word.replace(removeNonAlpha , '').split('').reverse().join('')
+  console.log(word === newTextWord)
+  return word === newTextWord
 }
+
+isPalindrome('abba') 
 
 /* 
   Add your pseudocode here
+  **A palindrome is a word or sentence that’s spelled the same way both forward and backward, ignoring punctuation, case, and spacing
+  
+  steps
+  ****remove all non-alphanumeric characters in the string;
+  ****turn everything lower case;
+  ****remove all white spaces if available => into a new array
+  ****reverse the new array/string
+  ****join the array into a string
+  ****compare the new string with the original one
+  ****if they are equal 
+         return true
+      otherwise 
+         return false
 */
 
 /*
